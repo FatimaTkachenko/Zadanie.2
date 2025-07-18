ff7e2c19f2ba173b9caeb426da66c5348df5d1a6
Fatima <fat.tka4enko2014@yandex.ru>
2025-07-18 16:06:15 +0300

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,17 @@
+public class Main {
+    public static void main(String[] args) {
+
+        int a = 200;// начальный счета
+        int b = 1750; // суммы пополнения
+        System.out.println(a + b); // сумма на счету
+
+        int result;
+        if (a + b > 1000) {
+            result = 1;
+        } else {
+            result = 0;
+        }
+        System.out.println(1950 / 100 + 1950);
+
+    }
+}
