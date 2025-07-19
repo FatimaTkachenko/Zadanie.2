e1d73a21ca415049c2354c3d8bdd4a8b610cc0d6
Fatima <fat.tka4enko2014@yandex.ru>
2025-07-19 15:25:22 +0300

    Create README.md

--- src/Main.java
+++ src/Main.java
@@ -1,12 +1,12 @@
 public class Main {
     public static void main(String[] args) {
 
-        int score = 200;// начальный счета
+        int remains = 200;// начальный счета
         int accrual = 1750; // суммы пополнения
-        System.out.println(score + accrual); // сумма на счету
+        System.out.println(remains + accrual); // сумма на счету
 
         int result;
-        if (score + accrual > 1000) {
+        if (remains + accrual > 1000) {
             result = 1;
         } else {
             result = 0;
