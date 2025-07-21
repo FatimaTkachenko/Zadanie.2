51f11672ea33197677b41ba6560009e6ac159755
Fatima <fat.tka4enko2014@yandex.ru>
2025-07-21 16:26:17 +0300

    Create README.md

--- src/Main.java
+++ src/Main.java
@@ -12,6 +12,6 @@ public class Main {
             result = 0;
         }
         System.out.println(1950 / 100);
-        System.out.println(1950 /100 +1950);
+        System.out.println(1950 / 100 +1950);
     }
 }
